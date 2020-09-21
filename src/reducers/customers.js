eb122e01a939583676ffbd0021613e7b21d0afcf
doradovirtual <64748488+doradovirtual@users.noreply.github.com>
2020-09-21 13:21:59 -0500

    Update customers.js

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -2,7 +2,7 @@ import { handleActions } from 'redux-actions';
 import {FETCH_CUSTOMERS}  from './../constants/index';
 
  export const customers = handleActions( 
-   {[FETCH_CUSTOMERS]: (action) => [...action.payload],
+   {[FETCH_CUSTOMERS]: (state, action) => [...action.payload],
      }, []);
 
 
